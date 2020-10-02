1722509de4e0857f70b76e85b0e3c87ce60a7808
Luke Sheppard <luke@luke-sheppard.com>
2020-10-02 19:03:30 +0100

    Remove all unused auth routes
    Password resets and email confirmation functionality
    is not needed, and therefore has been removed

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Auth::routes();
+// Authentication
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+
+// Registration
+Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+Route::post('register', 'Auth\RegisterController@register');
 
 Route::get('/', 'HomeController@index')->name('home');
 
