d462dd13f40a99b2ce70184fa5c8970fdc5a3514
Luke Sheppard <luke@luke-sheppard.com>
2020-10-03 12:09:29 +0100

    Add template feed configuration file
    Configuration values in this file have a default value,
    therefore do not need to be populated here unless the values
    should be different to the default.

---
+++ config/feed.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Configuration for feed parameters. Available configuration keys:
+ * cache_dir: Directory to store cached files
+ * max_cache_age: TTL for cached files in seconds. After this age, the cached feed will be reloaded.
+ */
+
+return [
+    //
+];
